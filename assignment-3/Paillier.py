0f86202594bb42110df8a8d8b62454fa45359c00
VedTheKnight <132833155+VedTheKnight@users.noreply.github.com>
2023-05-23 22:03:34 +0530

    Paillier Working code + README updates

---
+++ assignment-3/Paillier.py
@@ -0,0 +1,80 @@
+import random
+import json
+#this version works for small messages - error in prime generation not k-bit
+#issue in encryption -- very slow for large values!
+class Paillier:
+
+    def __init__(self,k = 1024):
+        self.k = k
+        #useful function
+        def L(x,n):
+            return (x-1)//n #note this is floor division in order to avoid overflow error
+
+        while(True):
+            #two distinct primes such that gcd(pq,(p-1)(q-1)) = 1
+            self.p = int(random_prime(2**self.k))
+            while(True):
+                self.q = int(random_prime(2**self.k))
+                if(self.p==self.q or gcd(self.p*self.q,(self.p-1)*(self.q-1)) != 1):continue
+                else:break
+            #computes n=pq
+            self.n = self.p*self.q
+            #computes lambda = lcm(p-1,q-1) ---> private key
+            self.l = lcm(self.p-1,self.q-1)
+            #computes g=random number in {1,2,3,..n^2-1}
+            self.g = randint(1,self.n**2-1)
+            #computes mu - modular multiplicative inverse
+            if(gcd(L(int(power_mod(self.g,self.l,self.n**2)),self.n),self.n)==1):
+                self.mu = inverse_mod(int(L(int(power_mod(self.g,self.l,self.n**2)),self.n)),self.n)
+                break
+            else:
+                continue
+
+        #random number in {1,2,3,...,n-1}
+        self.r = randint(1,self.n-1)
+
+
+    def get_public_key(self):
+        return self.n,self.g
+
+    def encrypt(self,plaintext):
+        plaintext_tuple = json.loads(plaintext.decode('utf-8'))
+        plaintext_int = int.from_bytes(plaintext_tuple[0].encode('utf-8'), 'big')
+
+        if(plaintext_int >= int(plaintext_tuple[1])):
+            raise ValueError("Invalid Message! Please try again with a smaller message.")
+        else:
+            #ciphertext_int = int(mod(int(plaintext_tuple[2]**plaintext_int)*int(self.r**plaintext_tuple[1]),plaintext_tuple[1]**2))#integer
+            ciphertext_int = int(mod(power_mod(plaintext_tuple[2], plaintext_int, plaintext_tuple[1] ** 2) * power_mod(self.r,plaintext_tuple[1],plaintext_tuple[1] ** 2),plaintext_tuple[1] ** 2))
+            ciphertext_bytes = ciphertext_int.to_bytes((ciphertext_int.bit_length() + 7) // 8, 'big')
+            return ciphertext_bytes
+
+    def decrypt(self,ciphertext):
+        ciphertext_int = int.from_bytes(ciphertext, 'big')
+
+        # useful function
+        def L(x, n):
+            return (x - 1) // n #note this is floor division in order to avoid overflow error
+
+        if(ciphertext_int>=self.n**2):
+            raise ValueError("Invalid Message! Please try again with a smaller message.")
+        else:
+            pm = power_mod(int(ciphertext_int),self.l,self.n*self.n)
+            L_val = L(pm,self.n)
+            plaintext_int = int(mod(int(L_val)*self.mu,self.n))
+            plaintext_bytes = plaintext_int.to_bytes((plaintext_int.bit_length() + 7) // 8, 'big')
+            return plaintext_bytes
+
+alice = Paillier(k=1024)
+bob = Paillier(k=1024)
+
+n_local,g_local = alice.get_public_key()
+message = "hello"
+plaintext = (message,int(n_local),int(g_local))
+
+plaintext = json.dumps(plaintext).encode('utf-8')
+
+ciphertext = bob.encrypt(plaintext)
+
+decrypted_message = alice.decrypt(ciphertext)
+print(f"Plaintext : {decrypted_message.decode('utf-8')}")
