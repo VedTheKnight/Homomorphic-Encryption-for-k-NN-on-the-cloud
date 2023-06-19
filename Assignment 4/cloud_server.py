92b65b550b7cec4884d0725ca780f77ad96198e5
VedTheKnight <132833155+VedTheKnight@users.noreply.github.com>
2023-06-19 18:49:55 +0530

    Assignment 4 + small changes to previous assignments
    Uploaded Assignment 4 and changed file names

---
+++ Assignment 4/cloud_server.py
@@ -0,0 +1,63 @@
+import socket
+import json
+
+#function to identify whether n is a prime: returns 1 if n is prime and 0 if it is not prime
+def isPrime(n):
+    for i in range(2,n):
+        if(n%i==0):
+            return 0
+    return 1
+
+#function to compute the next prime given a prime n
+def nextPrime(n):
+    n += 1
+    while(not isPrime(n)):
+        n += 1
+    return n
+
+#function to factorize n and return an array consisting of each prime factor
+def factorize(n):
+    factors = []
+    i=2
+    while (n > 1):
+        while(n % i == 0):
+            factors.append(i)
+            n = n / i
+        i = nextPrime(i)
+    return factors
+
+if __name__ == '__main__':
+
+    PORT2 = 65433 #port for the cloud server
+    SERVER = socket.gethostname()
+    ADDR2 = (SERVER,PORT2)
+    FORMAT = 'utf-8'
+
+    cloudServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #creates the cloud server socket
+    cloudServer.bind(ADDR2)
+    cloudServer.listen()
+    print(f"[CLOUD SERVER]Cloud Server is listening on {ADDR2}")
+
+    while True:
+        conn, addr = cloudServer.accept()
+        print(f"[CLOUD SERVER]{addr} connected. ")
+
+        connected = True
+        while connected:
+
+            data = json.loads(conn.recv(2048).decode(FORMAT)) #data obtained and converted into a dictionary
+            print("[CLOUD SERVER]Data Received")
+
+            integer = data[data["key"]] #extracts the modified integer from the data
+
+            print("[CLOUD SERVER]Running operations on data")
+            data[data["key"]] = factorize(integer)
+
+            with open("/vol/prime_factorization.txt", "a") as file:
+                file.write(f"{data[data['key']]}\n")
+
+            print("[CLOUD SERVER]Operations on the data are completed and uploaded to the volume mount")
+
+            connected = False
+
+        print(f"[CLOUD SERVER]{addr} has disconnected.")
---
+++ Assignment 4/query_user.py
@@ -0,0 +1,46 @@
+import socket
+import json
+
+if __name__ == '__main__':
+
+    PORT1 = 65432 #data owner port
+    PORT2 = 65433 #cloud server port
+    SERVER = socket.gethostname()
+    ADDR1 = (SERVER,PORT1)#data owner
+    ADDR2 = (SERVER,PORT2)#cloud server
+    FORMAT = 'utf-8'
+
+    with open("query_data.txt", "r") as file:
+        integer = int(file.readline())
+    query = {"key" : "password","password" : integer} #creates the query dictionary using the integer provided by the user
+
+    client = socket.socket(socket.AF_INET,socket.SOCK_STREAM) #intializes the client object
+
+    #interaction with the data owner
+
+    client.connect(ADDR1) #establishes connection with the data owner's port
+    client.sendall(json.dumps(query).encode(FORMAT)) #sends the query to the data owner for modification
+
+    data = json.loads(client.recv(2048).decode(FORMAT)) #obtains the modified query as data by multiplying the integer by a random number between 1 and 10000
+
+    print(f'[QUERY USER]Modified integer:{data[data["key"]]}')
+
+    client.close() #closes the connection with the data owner
+
+    #interaction with the cloud server
+
+    client = socket.socket(socket.AF_INET,socket.SOCK_STREAM) #re-initializes the client object for connection with the cloud server's port
+
+    client.connect(ADDR2) #establishes connection with the cloud server's port
+    client.sendall(json.dumps(data).encode(FORMAT)) #sends the data to the cloud server for prime factorization
+
+    print("[QUERY USER]Data sent to cloud server for computation")
+
+
+
+
+
+
+
+
+
